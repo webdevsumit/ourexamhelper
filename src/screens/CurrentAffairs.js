a36330e12dd4f61f3ebca50966855e64c27db220
Sumit Dhakad <sumitdhakad2232@gmail.com>
2021-08-14 07:48:57 +0530

    website complete

--- src/screens/CurrentAffairs.js
+++ src/screens/CurrentAffairs.js
@@ -1,5 +1,6 @@
 import React,{useState, useEffect} from 'react';
 import DateCard from "./components/DateCard";
+import "./css/CurrentAffairs.css"
 import fire from "./../fire";
 
 function CurrentAffairs(){
@@ -14,9 +15,10 @@ function CurrentAffairs(){
 		ref.get().then(querySnapshot=>{
 			const items = [];
 			querySnapshot.forEach(doc=>{
-				items.push(doc.data());
+				items.push({'id':doc.id, 'data': doc.data()});
 			});
-			setData(items);
+			
+			setData(items.reverse());
 			setLoading(false);
 		}).catch(err=>{
 			alert(err);
@@ -27,19 +29,52 @@ function CurrentAffairs(){
 		getData();
 	},[]);
 
+	const [details, setDetails] = useState(false);
+	
+	
+	const getDetails=(id, Name, Image, date)=>{
+			ref.doc(id).collection('Details').get().then(querySnapshot=>{
+				const items = [];
+				 querySnapshot.forEach(doc=>{
+					 items.push(doc.data());
+				 });
+				
+				setDetails({
+					'id':id,
+					'Name':Name,
+					'Image':Image,
+					'date':date,
+					'details':items
+				});
+			}).catch(err=>{
+				alert(err);
+			});
+		};
+	
+
 	if(loading) return(<div><h5>loading...</h5></div>);
 	
 	return(
-		<div>
-			<h3>CURRENT AFFAIRS</h3>
-			<div>
+		<div className="current-affairs">
+			<h3 className="current-affairs-head">CURRENT AFFAIRS</h3>
+			<div className="cards">
 				{data.map(d=><DateCard
 					key={d.id}
-					PostDate={d.Date.seconds}
-					Image={d.Image}
-					Name={d.Name}
+					id={d.id}
+					PostDate={d.data.Date.seconds}
+					Image={d.data.Image}
+					Name={d.data.Name}
+					click={getDetails}
 					/>)}
 			</div>
+			{details && <div className='detailed-page'>
+				<h3>{details.Name}</h3>
+				<h6><em>{details.date}</em></h6>
+				<img src={details.details[0].Image}/>
+				<button onClick={()=>setDetails(false)}>X</button>
+				<p>{details.details[0].Detail}</p>
+				<img src={details.Image}/>
+			</div>}
 		</div>
 	)
 }
--- src/screens/components/DateCard.js
+++ src/screens/components/DateCard.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React,{useState} from 'react';
 import "./../css/DateCard.css"
+
 
 function DateCard(props){
 	const fDate = new Date(props.PostDate*1000);
 	const date = fDate.getDate();
-	const month = fDate.getMonth()+1;
+	const month = fDate.getMonth();
 	const year = fDate.getFullYear();
+	const monthNames = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+	
+	
 	return(
-		<div className="date-card">
+		<div className="date-card" onClick={
+			()=>props.click(
+				props.id,
+				props.Name,
+				props.Image,
+				date+' '+monthNames[month]+' '+year
+			)
+		}>
+			<p className="day">{date}</p>
+			<p><em>{monthNames[month]+" "+year}</em></p>
 			<img src={props.Image} alt="back img"/>
-			<h3>{props.Name}</h3>
-			<p><em>{date+"/"+month+"/"+year}</em></p>
+			<h6>{props.Name}</h6>
 		</div>
 	)
 }
